e67b45aad285a9257c340f13793e70fa9c38a033
endemedia <endemedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-18 21:01:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/website-thumbnails-with-easy-tags/trunk@87583 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- picdonkey.php
+++ picdonkey.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: picdonkey
 Plugin URI: http://www.bildesel.de/
-Description: Thumbnails von Homepages mit Hilfe vom Bildesel in BLOG Artikeln anzeigen. Einfach mit [thumb]http://www.homepage.de[/thumb]
+Description: Show Thumbnails of webpages just with a tag: Use [thumb]http://www.homepage.com[/thumb] in you articles and you have linked thumbnails.
 Version: 1.0
 Author: Sascha Ende
 Author URI: http://www.bildesel.de/
